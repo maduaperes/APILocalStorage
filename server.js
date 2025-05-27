f98d607251afcafccd6af7ee2ec80d3fae170342
Maria Eduarda <mariaeduardaaperes@hotmail.com>
2025-05-27 20:00:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,123 @@
-// importa o framework express
 const express = require('express');
-// importa o modulo cors para permitir requisições de outras origens
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://usuarioTeste:Teste123@cluster0.wl8ayaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const cors = require('cors');
-// cria uma instancia da aplicação express
 const app = express();
+const port = process.env.PORT || 3000;
+const nomeBanco = 'dados';
+const nomeColecao = 'App';
 
-// define a porta em que a aplicação vai rodar
-const PORT = 3000;
+let db;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-//aplica o middleware cors para permitir requisições de outras origens
+async function run() {
+    try {
+        await client.connect();
+        db = client.db(nomeBanco);
+
+    } finally {
+        await client.close();
+    }
+}
 app.use(cors());
-//aplica o middleware express.json que permite receber e interpretar JSON no corpo das requisições(red.body)
 app.use(express.json());
+app.use(express.static('public'));
 
-//Array em memoria para simular o banco de dados
-let tarefas = [];
+//API ADICIONAR
+app.post('/api/items', async (req, res) => {
+    try {
+        const { nome, descricao } = req.body;
 
-
-/* --------------------------ROTAS DA API-------------------------- */
-
-// rota GeT - RETORNA A LISTA COM TODAS AS TAREFAS
-app.get('/tarefas', (req, res) => {
-    res.json(tarefas);
-});
-// Rota POST -adiciona uma tarefa a lista
-app.post('/tarefas', (req, res) => {
-    const { texto } = req.body;
-    const novaTarefa = { id: Date.now(), texto };
-    tarefas.push(novaTarefa);
-    res.status(201).json(novaTarefa);
-});
-
-// Rota PUT -atualiza uma tarefa da lista
-app.put('/tarefas/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const { texto } = req.body;
-    const index = tarefas.findIndex(tarefa => tarefa.id === id);
-    if (!index) {
-        return res.sendStatus(404).json({ error: 'Tarefa nao encontrada' });
-    } if (texto) {
-        index.texto = texto;
-    } else {
-        index.texto = index.texto
+        if (!nome) {
+            return res.status(400).json({ message: 'O campo nome não foi preenchido' });
+        }
+        const novoItem = { nome, descricao: descricao || '', createdAt: new Date() };
+        const resultado = await db.collection(nomeColecao).insertOne(novoItem);
+        const inserirItem = await db.collection(nomeColecao).findOne({ _id: resultado.insertedId });
+        res.status(201).json(inserirItem);
+    } catch (error) {
+        console.error("Erro ao adicionar items:", error);
+        res.status(500).json({ message: 'Erro interno do servidor ao inserir dados' });
     }
-    // atualiza o texto da tarefa encontrada
-    tarefas[index].texto = texto;
-    res.json(tarefas[index]);
 });
 
-// Rota DELETE -remove uma tarefa da lista
-app.delete('/tarefas/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = tarefas.find(tarefa => tarefa.id == id);
-    if (!index) {
-        return res.sendStatus(404).json({ error: 'Tarefa nao encontrada' });
+//API LEITURA
+app.get('/api/items', async (req, res) => {
+    if(!db){
+        return res.status(503).json({message:'Serviço indisponivel (Banco de Dados)'});
     }
-    tarfas = tarefas.filter(tarefa => tarefa.id != id);
-    res.sendStatus(204);
+    try {
+        const items = await db.collection(nomeColecao).find({}).sort({ createdAt: -1 }).toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        console.error("Erro ao buscar os dados", error);
+        res.status(500).json({ message: "Erro interno do servidor ao buscar dados" });
+    }
 });
 
-// inicia o servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+//API ATUALIZAR 
+app.put('/api/items/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'ID inválido' });
+        }
+        const { nome, descricao } = req.body;
+        if (!nome) {
+            return res.status(400).json({ message: 'O campo nome não foi preenchido' });
+        }
+        const atualizarDados = {
+            $set: {
+                nome, descricao: descricao || '', updatedAt: new Date()
+            }
+        };
+        const result = await db.collection(nomeColecao).findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            atualizarDados,
+            { returnDocument: 'after' }
+        );
+
+        if (!result || !result.value) {
+            return res.status(404).json({ message: 'Dado não encontrado para atualização' });
+        }
+        res.status(200).json(result.value);
+    } catch (error) {
+        console.error("Erro ao atualizar os dados", error);
+        res.status(500).json({ message: "Erro interno do servidor ao atualizar os dados" });
+    }
 });
 
+//API DELETAR
+
+app.delete('/api/items/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'ID inválido' });
+        }
+
+        const result = await db.collection(nomeColecao).deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: 'Dado não encotrado para ser deletado' });
+        }
+        res.status(200).json({message:"Item excluido com sucesso!"});
+    } catch (error) {
+        console.error("Erro ao deletar os dados", error);
+        res.status(500).json({ message: "Erro interno do servidor ao deletar os dados" });
+    }
+});
+
+async function iniciarServidor() {
+    run().catch(console.dir);
+    
+    app.listen(port, ()=>{
+        console.log(`Servidor rodando em http:\\localhost:${port}`);
+    });
+}
+iniciarServidor();
